8afd6118675b03fe80b726e21d63566970e3305c
Jamie Fujimoto <hayntopdawg@gmail.com>
2015-10-06 17:44:38 -0400

    Vectorize a*y, compute error, and compute bias

--- assign2-FUJIMOTO-JAMIE.py
+++ assign2-FUJIMOTO-JAMIE.py
@@ -16,31 +16,38 @@ def get_data(filename):
 
 
 def kernel_func(kernel, Xi, Xj):
-    Kij = np.dot(Xi.T, Xj)
+    Kij = np.dot(Xi, Xj)
     if kernel == 'linear':
         return Kij
     elif kernel == 'quadratic':
         return Kij**2
 
 
-def compute_ay(a, y, n):
+def compute_ay(a, y):
     """
     Used to vectorize sum([a[i] * y[i] for i in xrange(n)])
 
-    n can be number of samples or number of support vectors
+    Returns a vector using element-wise multiplication between vector a and y
     """
-    ay = np.array([a[i] * y[i] for i in xrange(n)])
-    print "ay: {0}".format(ay)
+    ay = np.multiply(a, y)
     return ay
 
 
 def compute_error(kernel, X, y, a, n, k):
-    return sum([a[j] * y[j] * kernel_func(kernel, X[j], X[k]) for j in xrange(n)]) - y[k]
+    """
+    Computes the error between the predicted value and the true value for the point X[k, :]
+
+    Returns a scalar
+    """
+    ay = compute_ay(a, y)
+    Kjk = kernel_func(kernel, X, X[k, :])
+    return np.dot(ay, Kjk) - y[k]
 
 
-def compute_bi(kernel, X, y, a, indexes, i):
-    S = sum([a[j] * y[j] * kernel_func(kernel, X[j], X[i]) for j in indexes])
-    return y[i] - S  # using Eq. (21.33)
+def compute_bi(kernel, X, y, a, i):
+    ay = compute_ay(a, y)
+    Kji = kernel_func(kernel, X, X[i, :])
+    return y[i] - np.dot(ay, Kji)  # using Eq. (21.33)
 
 
 def compute_weight(X, y, a, i):
@@ -117,47 +124,57 @@ if __name__ == "__main__":
     # Get support vector indexes
     indexes = [i for i in xrange(n) if a[i] > 0]
 
-    # Compute bias
-    bi_vec = [compute_bi(kernel, X, y, a, indexes, i) for i in indexes]
-    bias = sum(bi_vec) / len(bi_vec)
+    ai = np.array([a[i] for i in indexes])  # Create ai vector
+    print "ai.shape: {0}".format(ai.shape)
+    yi = np.array([y[i] for i in indexes])  # Create yi vector
+    print "yi.shape: {0}".format(yi.shape)
+    Xi = np.array([X[i, :] for i in indexes])  # Create Xi matrix
+    print "Xi.shape: {0}".format(Xi.shape)
 
-    # Compute w if linear kernel
-    if kernel == "linear":
-        weights = [compute_weight(X, y, a, i) for i in indexes]
-        d = X.shape[1]
-        weight = np.zeros(d)
-        for w in weights:
-            for i in xrange(d):
-                weight[i] += w[i]
+    aiyi = compute_ay(ai, yi)
+    print "aiyi.shape: {0}".format(aiyi.shape)
 
+    # # Compute bias
+    # bi_vec = [compute_bi(kernel, X, y, a, i) for i in indexes]
+    # bias = sum(bi_vec) / len(bi_vec)
 
-    # Compute accuracy
-    pred = [predict(a, indexes, kernel, X, y, bias, X[i]) for i in xrange(n)]
-    count = 0
-    for i in xrange(n):
-        if pred[i] == y[i]:
-            count += 1
-    accuracy = count / n
-
-    with open("assign2-FUJIMOTO-JAMIE.txt", "w") as f:
-        # Print support vector indexes
-        print "The support vectors are:"
-        f.write("The support vectors are:\n")
-        for i in indexes:
-            print i, a[i]
-            f.write("{0} {1}\n".format(i, a[i]))
-        print "Number of support vectors: {0}\n".format(len(indexes))
-        f.write("Number of support vectors: {0}\n".format(len(indexes)))
-
-        # Print bias
-        print "Bias:", bias
-        f.write("Bias: {0}\n".format(bias))
-
-        # Print w for linear kernel
-        if kernel == "linear":
-            print "w:", weight
-            f.write("w: {0}\n".format(weight))
-
-        # Print accuracy
-        print "Accuracy:", accuracy
-        f.write("Accuracy: {0}\n".format(accuracy))
+    # # Compute w if linear kernel
+    # if kernel == "linear":
+    #     weights = [compute_weight(X, y, a, i) for i in indexes]
+    #     d = X.shape[1]
+    #     weight = np.zeros(d)
+    #     for w in weights:
+    #         for i in xrange(d):
+    #             weight[i] += w[i]
+    #
+    #
+    # # Compute accuracy
+    # pred = [predict(a, indexes, kernel, X, y, bias, X[i]) for i in xrange(n)]
+    # count = 0
+    # for i in xrange(n):
+    #     if pred[i] == y[i]:
+    #         count += 1
+    # accuracy = count / n
+    #
+    # with open("assign2-FUJIMOTO-JAMIE.txt", "w") as f:
+    #     # Print support vector indexes
+    #     print "The support vectors are:"
+    #     f.write("The support vectors are:\n")
+    #     for i in indexes:
+    #         print i, a[i]
+    #         f.write("{0} {1}\n".format(i, a[i]))
+    #     print "Number of support vectors: {0}\n".format(len(indexes))
+    #     f.write("Number of support vectors: {0}\n".format(len(indexes)))
+    #
+    #     # Print bias
+    #     print "Bias:", bias
+    #     f.write("Bias: {0}\n".format(bias))
+    #
+    #     # Print w for linear kernel
+    #     if kernel == "linear":
+    #         print "w:", weight
+    #         f.write("w: {0}\n".format(weight))
+    #
+    #     # Print accuracy
+    #     print "Accuracy:", accuracy
+    #     f.write("Accuracy: {0}\n".format(accuracy))
